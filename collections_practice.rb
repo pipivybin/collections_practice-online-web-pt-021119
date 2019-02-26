8b9f3130adab422bd4f84a1196e216468335112f
Ming Duan <11203duan@gmail.com>
2019-02-26 07:05:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,58 @@
 
+
+def sort_array_asc(arr)
+  arr.sort
+end
+
+def sort_array_desc(arr)
+  arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+  arr.sort do
+    |a, b| a.length <=> b.length
+  end
+  arr
+end
+
+def swap_elements(arr)
+  a = arr[2]
+  b = arr[3]
+  arr[2] = b
+  arr[3] = a
+  arr
+end
+
+def reverse_array(arr)
+  arr.sort.reverse
+end
+
+def kesha_maker(arr)
+  arr.each do
+    |x| x[2] = "$"
+  end
+  arr
+end
+
+def find_a(arr)
+  arry = []
+  arr.each do
+    |x| arry << x if x[0] = "a"
+  end
+  arry
+end
+
+def sum_array(arr)
+  sum = 0
+  arr.each do
+    |x| sum += x
+  end
+  sum
+end
+
+def add_s(arr)
+  arr.each do
+    |x| x.insert(-1, "s")
+  end
+  arr
+end
